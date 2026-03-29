6b8d4c084a3898266f8a1945ee9635450a3bf1a1
ankitsinghchandrawat <achandrawat345@gmail.com>
2026-03-29 06:12:25 +0000

    cleaned project

--- main.rb
+++ main.rb
@@ -2,36 +2,10 @@ require_relative 'lib/item'
 require_relative 'lib/tax_calculator'
 require_relative 'lib/receipt'
 
-puts "Enter items (type 'done' to finish):"
-
-items = []
-
-while true
-  input = gets.chomp
-  break if input == "done"
-
-  parts = input.split(" ")
-
-  quantity = parts[0].to_i
-  price = parts[-1].to_f
-  name = parts[1..-3].join(" ")
-
-  category = "other"
-  if name.include?("book")
-    category = "book"
-  elsif name.include?("chocolate")
-    category = "food"
-  elsif name.include?("pill")
-    category = "medical"
-  end
-
-  imported = name.include?("imported")
-
-  item = Item.new(name, price, category, imported)
-  items << item
-end
-
-calculator = TaxCalculator.new
-receipt = Receipt.new(items, calculator)
-
-receipt.print
+item1 = Item.new("book", 12.49,"book" ,false)
+item2 = Item.new("music CD", 14.99, "other", false)
+item3 = Item.new("chocolate bar", 0.85, "food", false)
+ items = [item1, item2, item3]
+ calculator = TaxCalculator.new
+ receipt = Receipt.new(items, calculator)
+ receipt.print
